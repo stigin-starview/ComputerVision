c12d7f362712dab00432218c246ac89ef0645e71
stigin <stiginstaview@hotmail.com>
2021-12-21 10:37:08 +0530

    resize image and save the resized image

--- script.py
+++ script.py
@@ -3,6 +3,10 @@ import cv2
 # Reading image file.
 img = cv2.imread("resources/drum.jpg",0)
 
-cv2.imshow("drums",img)
-cv2.waitKey(10000)
+print(img.shape)
+
+resized_image = cv2.resize(img, (int(img.shape[1]/3),int(img.shape[0]/3)))
+cv2.imshow("drums",resized_image)
+cv2.imwrite("drums_resized.jpg", resized_image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
